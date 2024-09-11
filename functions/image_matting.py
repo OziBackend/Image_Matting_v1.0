73aad65605c8656032103448edd43d331fd33c6e
OziBackend <ozibackend@gmail.com>
2024-09-11 10:39:28 +0500

    Image Format changed to webp

--- functions/image_matting.py
+++ functions/image_matting.py
@@ -13,11 +13,11 @@ for folder in [output_folder, foreground_folder]:
 
 def image_matting(app, file_path, file, return_data):
     with app.app_context():
-    # Run inference
+        # Run inference
         os.system(f'python -m demo.image_matting.colab.inference '
-              f'--input-path {file_path} '
-              f'--output-path {output_folder} '
-              f'--ckpt-path {pretrained_ckpt}')
+                  f'--input-path {file_path} '
+                  f'--output-path {output_folder} '
+                  f'--ckpt-path {pretrained_ckpt}')
 
         # Load and process the image
         matte_name = file.filename.split('.')[0] + '.png'
@@ -25,20 +25,19 @@ def image_matting(app, file_path, file, return_data):
         matte = Image.open(os.path.join(output_folder, matte_name))
         rgba_foreground_image = generate_rgba_foreground(app, image, matte, file.filename)
 
-        # Save the foreground image to the static folder
-        foreground_image_name = f'foreground_{file.filename}'
+        # Save the foreground image to the static folder in WebP format
+        foreground_image_name = f"foreground_{file.filename.split('.')[0]}.webp"
         foreground_image_path = os.path.join(foreground_folder, foreground_image_name)
-        rgba_foreground_image.save(foreground_image_path, format='PNG')
+        rgba_foreground_image.save(foreground_image_path, format='WEBP')
 
         # Generate the URL for the saved image
         image_url = url_for('static', filename=f'foreground/{foreground_image_name}', _external=True)
 
-        return_data['image_url']={"image_url": image_url}
-
+        return_data['image_url'] =  image_url
 
 def generate_rgba_foreground(app, image, matte, image_name):
     with app.app_context():
-        # Calculate display resolution
+        # Convert image to numpy array
         image = np.asarray(image)
         if len(image.shape) == 2:
             image = image[:, :, None]
@@ -46,14 +45,13 @@ def generate_rgba_foreground(app, image, matte, image_name):
             image = np.repeat(image, 3, axis=2)
         elif image.shape[2] == 4:
             image = image[:, :, 0:3]
-        matte = np.repeat(np.asarray(matte)[:, :, None], 3, axis=2) / 255
+        matte = np.asarray(matte) / 255  # Ensure matte is in [0, 1] range
 
         # Calculate the foreground with the original color
-        foreground = image * matte + np.full(image.shape, 255) * (1 - matte)
+        foreground = image * matte[:, :, None] + np.full(image.shape, 255) * (1 - matte[:, :, None])
 
-        # Convert the foreground to an RGBA image (with transparency)
+        # Convert to an RGBA image (with transparency)
         foreground_image = Image.fromarray(np.uint8(foreground)).convert('RGB')
-        alpha_matte = Image.fromarray(np.uint8(matte[:, :, 0] * 255)).convert('L')
+        alpha_matte = Image.fromarray(np.uint8(matte * 255)).convert('L')
         rgba_foreground_image = Image.merge('RGBA', (foreground_image.split()[0], foreground_image.split()[1], foreground_image.split()[2], alpha_matte))
-        
         return rgba_foreground_image
